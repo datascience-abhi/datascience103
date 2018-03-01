4ff2b8fd32cfd81fe0fb57d62db906d267cd59fb
Shetty <Abhijith.Shetty@cox.com>
2018-03-01 11:12:19 -0500

    new primenumber file

---
+++ primenumber_test.r
@@ -0,0 +1,16 @@
+ prime = 0:100
+    temp = 0
+    for(val in prime){
+      if (val == 0){
+        next
+      } else if (val == 1){
+        next
+      } else if (val == 2){
+        TRUE
+        temp = val 
+      } else if (val %% temp == 0){
+        next
+        temp = temp + 1
+      }
+      print(val)
+    }
